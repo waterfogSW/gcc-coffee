3187684d1bcf5131909f7b0874a1b67ce4958a75
San Kim <waterfog9580@gmail.com>
2022-05-03 20:01:07 +0900

    :white_check_mark: findAllProduct 테스트 코드 작성

--- src/test/java/com/waterfogsw/gcccoffee/product/DefaultProductServiceTests.java
+++ src/test/java/com/waterfogsw/gcccoffee/product/DefaultProductServiceTests.java
@@ -12,6 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -64,6 +68,29 @@ public class DefaultProductServiceTests {
                 assertThat(addedProduct.getDescription(), is(product.getDescription()));
                 assertThat(addedProduct.getCreatedAt(), is(product.getCreatedAt()));
                 assertThat(addedProduct.getUpdatedAt(), is(product.getUpdatedAt()));
+            }
+        }
+    }
+    
+    @Nested
+    @DisplayName("findAllProduct 메서드는")
+    class Describe_findAllProduct {
+    
+        @Nested
+        @DisplayName("호출되면")
+        class Context_with_call {
+            
+            @Test
+            @DisplayName("저정된 모든 제품 List 를 반환한다")
+            void it_product_list() {
+                final var product1 = new Product(1L, "product1", Category.COFFEE_GRINDER, 10000, "");
+                final var product2 = new Product(2L, "product2", Category.COFFEE_GRINDER, 12000, "");
+                final List<Product> products = new ArrayList<>(Arrays.asList(product1, product2));
+
+                when(productRepository.selectAll()).thenReturn(products);
+
+                final var findProducts = productService.findAllProduct();
+                assertThat(findProducts.size(), is(2));
             }
         }
     }
