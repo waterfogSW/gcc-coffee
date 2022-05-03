721b629ec0b91dca3813ec8bd3a9ed9062df3efc
San Kim <waterfog9580@gmail.com>
2022-05-03 14:47:33 +0900

    :sparkles: ProductService addProduct 프로덕션 코드 작성

--- src/main/java/com/waterfogsw/gcccoffee/product/repository/ProductMemoryRepository.java
+++ src/main/java/com/waterfogsw/gcccoffee/product/repository/ProductMemoryRepository.java
@@ -1,4 +1,12 @@
 package com.waterfogsw.gcccoffee.product.repository;
 
+import com.waterfogsw.gcccoffee.product.model.Product;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductMemoryRepository implements ProductRepository {
+    @Override
+    public Product insertProduct(Product product) {
+        return null;
+    }
 }
--- src/main/java/com/waterfogsw/gcccoffee/product/service/DefaultProductService.java
+++ src/main/java/com/waterfogsw/gcccoffee/product/service/DefaultProductService.java
@@ -1,13 +1,24 @@
 package com.waterfogsw.gcccoffee.product.service;
 
 import com.waterfogsw.gcccoffee.product.model.Product;
+import com.waterfogsw.gcccoffee.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DefaultProductService implements ProductService {
 
+    private final ProductRepository productRepository;
+
+    public DefaultProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product addProduct(Product product) {
-        return null;
+        if (product == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return productRepository.insertProduct(product);
     }
 }
