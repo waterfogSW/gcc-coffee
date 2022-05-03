a796de5c422ad5ca67d3b22eb3c62e4b49b10070
San Kim <waterfog9580@gmail.com>
2022-05-03 14:47:33 +0900

    :sparkles: Feat: ProductService 프로덕션 코드 작성

--- src/main/java/com/waterfogsw/gcccoffee/product/repository/ProductRepository.java
+++ src/main/java/com/waterfogsw/gcccoffee/product/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.waterfogsw.gcccoffee.product.repository;
 
+import com.waterfogsw.gcccoffee.product.model.Product;
+
 public interface ProductRepository {
+    Product insertProduct(Product product);
 }
