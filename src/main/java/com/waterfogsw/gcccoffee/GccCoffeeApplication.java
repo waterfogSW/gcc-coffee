21c7b200175865b9d2309d930f77b6735662f212
San Kim <waterfog9580@gmail.com>
2022-05-03 14:15:59 +0900

    :tada: Init: 프로젝트 생성

---
+++ src/main/java/com/waterfogsw/gcccoffee/GccCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.waterfogsw.gcccoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GccCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GccCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waterfogsw/gcccoffee/GccCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterfogsw.gcccoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GccCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
