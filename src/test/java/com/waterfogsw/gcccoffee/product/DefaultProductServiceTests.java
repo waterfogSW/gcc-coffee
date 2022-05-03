c5980101cbdc68968f6ae30468729723772a9918
San Kim <waterfog9580@gmail.com>
2022-05-04 00:58:35 +0900

    :white_check_mark: findById 테스트코드 작성

--- src/test/java/com/waterfogsw/gcccoffee/product/DefaultProductServiceTests.java
+++ src/test/java/com/waterfogsw/gcccoffee/product/DefaultProductServiceTests.java
@@ -1,5 +1,6 @@
 package com.waterfogsw.gcccoffee.product;
 
+import com.waterfogsw.gcccoffee.exception.ResourceNotFound;
 import com.waterfogsw.gcccoffee.product.model.Category;
 import com.waterfogsw.gcccoffee.product.model.Product;
 import com.waterfogsw.gcccoffee.product.repository.ProductRepository;
@@ -15,11 +16,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -71,15 +75,15 @@ public class DefaultProductServiceTests {
             }
         }
     }
-    
+
     @Nested
     @DisplayName("findAllProduct 메서드는")
     class Describe_findAllProduct {
-    
+
         @Nested
         @DisplayName("호출되면")
         class Context_with_call {
-            
+
             @Test
             @DisplayName("저정된 모든 제품 List 를 반환한다")
             void it_product_list() {
@@ -91,6 +95,43 @@ public class DefaultProductServiceTests {
 
                 final var findProducts = productService.findAllProduct();
                 assertThat(findProducts.size(), is(2));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("findById 메서드는")
+    class Describe_findById {
+
+        @Nested
+        @DisplayName("존재하는 엔티티에 대한 요청이면")
+        class Context_with_exist_id {
+
+            @Test
+            @DisplayName("해당 엔티티를 반환한다")
+            void it_product_list() {
+                final var product = new Product.Builder(1L)
+                        .name("test")
+                        .category(Category.COFFEE_GRINDER)
+                        .price(1000)
+                        .build();
+
+                when(productRepository.selectById(anyLong())).thenReturn(Optional.of(product));
+
+                final var findProducts = productService.findById(1L);
+                assertThat(findProducts, samePropertyValuesAs(product));
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지 않는 엔티티에 대한 요청이면")
+        class Context_with_not_exist_id {
+
+            @Test
+            @DisplayName("ResourceNotFound 에러를 발생시킨다")
+            void it_product_list() {
+                when(productRepository.selectById(anyLong())).thenReturn(Optional.empty());
+                assertThrows(ResourceNotFound.class, () -> productService.findById(1L));
             }
         }
     }
