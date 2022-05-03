19d5a7cc125d5984adc4b650235551a50ae476ed
San Kim <waterfog9580@gmail.com>
2022-05-03 19:43:12 +0900

    :sparkles: findAll 프로덕션 코드 작성

---
+++ src/main/java/com/waterfogsw/gcccoffee/product/controller/api/SortType.java
@@ -0,0 +1,11 @@
+package com.waterfogsw.gcccoffee.product.controller.api;
+
+import com.waterfogsw.gcccoffee.product.controller.dto.ProductResponse;
+import com.waterfogsw.gcccoffee.product.model.Product;
+
+import java.util.Comparator;
+
+public enum SortType {
+    PRICE,
+    DATE
+}
