4e95af3dbcaffca2e7ff0b1a9b08babcf748218b
San Kim <waterfog9580@gmail.com>
2022-05-05 22:02:58 +0900

    :adhesive_bandage: order -> orders, 엔드포인트 복수형으로 변경

--- src/main/java/com/waterfogsw/gcccoffee/order/controller/api/OrderApiController.java
+++ src/main/java/com/waterfogsw/gcccoffee/order/controller/api/OrderApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orders")
 @RestController
 public class OrderApiController {
     private final OrderService orderService;
