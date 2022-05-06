31897c44eaabde162da4787f05df4173cd40055d
San Kim <waterfog9580@gmail.com>
2022-05-07 02:17:05 +0900

    :sparkles: product-detail 페이지 추가

--- src/main/java/com/waterfogsw/gcccoffee/product/controller/ProductAdminController.java
+++ src/main/java/com/waterfogsw/gcccoffee/product/controller/ProductAdminController.java
@@ -1,11 +1,13 @@
 package com.waterfogsw.gcccoffee.product.controller;
 
 import com.waterfogsw.gcccoffee.product.controller.dto.ProductAddRequest;
+import com.waterfogsw.gcccoffee.product.controller.dto.ProductResponse;
 import com.waterfogsw.gcccoffee.product.model.Product;
 import com.waterfogsw.gcccoffee.product.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -35,5 +37,12 @@ public class ProductAdminController {
     public String newProduct(ProductAddRequest request) {
         productService.addProduct(Product.from(request));
         return "redirect:/products";
+    }
+
+    @GetMapping("/products/{id}")
+    public String viewProductPage(@PathVariable long id, Model model) {
+        final var product = productService.findById(id);
+        model.addAttribute("product", ProductResponse.from(product));
+        return "product";
     }
 }
