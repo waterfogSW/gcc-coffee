a34757f2cf08e8dc35a8ecd2f678bc9ea1574722
San Kim <waterfog9580@gmail.com>
2022-05-06 12:44:48 +0900

    :sparkles: front ui 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React, {useEffect, useState} from 'react'
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios"
 
 function App() {
+  const [products, setProducts] = useState([
+    {id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 3000},
+    {id: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 3000},
+    {id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 3000},
+  ]);
+
+  const [items, setItems] = useState([]);
+  const handleAddClicked = productId => {
+    const product = products.find(v => v.id === productId);
+    const found = items.find(v => v.id === productId);
+    const updatedItems =
+        found ? items.map(v => (v.id === productId) ? {...v, count: v.count + 1} : v) :
+            [...items, {...product, count: 1}]
+    setItems(updatedItems);
+    console.log(products.find(v => v.id === productId), "added");
+  }
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/products')
+        .then(v => setProducts(v.data));
+  }, [])
+  const handleOrderSubmit = (order) => {
+    if (items.length === 0) {
+      alert("아이템을 추가해 주세요!");
+    } else {
+      axios.post('http://localhost:8080/api/v1/orders', {
+        email: order.email,
+        address: order.address,
+        postcode: order.postcode,
+        orderProducts: items.map(v => ({
+          id: v.id,
+          category: v.category,
+          price: v.price,
+          quantity: v.count
+        }))
+      }).then(v => alert("주문이 정상적으로 접수되었습니다"), e => {
+        alert("서버 장애");
+        console.error(e);
+      })
+    }
+    console.log(order, items)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid">
+        <div className="row justify-content-center m-4">
+          <h1 className="text-center">GNU Coffee Collection</h1>
+        </div>
+        <div className="card">
+          <div className="row">
+            <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+              <ProductList products={products} onAddClick={handleAddClicked}/>
+            </div>
+            <div className="col-md-4 summary p-4">
+              <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
